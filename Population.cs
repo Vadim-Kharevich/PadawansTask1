62e0f5d47f646562c85faa996ec1d493e41f0189
Vadim-Kharevich <41491334+Vadim-Kharevich@users.noreply.github.com>
2019-02-12 11:52:26 +0300

    Update Population.cs

--- Population.cs
+++ Population.cs
@@ -6,8 +6,13 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            int countyears = 0;
+            while (initialPopulation < currentPopulation)
+            {
+                initialPopulation += Convert.ToInt32(initialPopulation / 100 * percent)+visitors;
+                countyears++;
+            }
+            return countyears;
         }
     }
 }
